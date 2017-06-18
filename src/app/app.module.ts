936739a9d956bf173fe2f612d44ccd720a1b1706
marceliino <marceliino1@gmail.com>
2017-06-17 23:29:24 -0300

    commit inicial

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ContatoComponent } from './contato/contato.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { HttpModule } from '@angular/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContatoComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
